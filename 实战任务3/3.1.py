4645d70477e51c07f63ac4960e2b7049bf18bf26
Yu'sDesktop <1526892925@qq.com>
2024-01-11 22:02:20 +0800

    实战3

---
+++ 实战任务3/3.1.py
@@ -0,0 +1,6 @@
+# 开发人员    :  Yu
+# 开发时间    : 2024/1/11  21:48
+# 文件名称    : 3.1.PY
+# 开发工具    : PyCharm
+name = input("检测键盘上的输入： ")
+print(name+" 的ASCII码为： ",ord(name))
---
+++ 实战任务3/3.4.py
@@ -0,0 +1,6 @@
+# 开发人员    :  Yu
+# 开发时间    : 2024/1/11  21:58
+# 文件名称    : 3.4.PY
+# 开发工具    : PyCharm
+import datetime
+nowday=datetime.date.today()
