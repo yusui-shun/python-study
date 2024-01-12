027caf29491da43a4af5415aa60a3403b52c68ba
Yu'sDesktop <1526892925@qq.com>
2024-01-12 11:35:27 +0800

    完成未完成的3.4和3.5

--- 实战任务3/3.4.py
+++ 实战任务3/3.4.py
@@ -2,5 +2,17 @@
 # 开发时间    : 2024/1/11  21:58
 # 文件名称    : 3.4.PY
 # 开发工具    : PyCharm
-import datetime
-nowday=datetime.date.today()
+from datetime import datetime
+day20=datetime.strptime('2024-11-11 0:0:0','%Y-%m-%d %H:%M:%S')
+#设置未来时间
+now=datetime.today()
+delta=day20-now
+day=delta.days
+hour=int(delta.seconds /60 /60)
+minutes=int((delta.seconds-hour*60*60)/60)
+seconds=delta.seconds-hour*60*60-minutes*60
+print('\033[31;43m 距离结束: \033[43m'+'\033[34;43m'+str(day)+'\033[43m'+'\033[31;43m天 \033[43m'+
+      '\033[31;43m 距离结束: \033[43m'+'\033[34;43m'+str(hour)+'\033[43m'+'\033[31;43m小时 \033[43m'+
+      '\033[31;43m 距离结束: \033[43m'+'\033[34;43m'+str(minutes)+'\033[43m'+'\033[31;43m分钟 \033[43m'+
+      '\033[31;43m 距离结束: \033[43m'+'\033[34;43m'+str(seconds)+'\033[43m'+'\033[31;43m秒 \033[43m')
+
---
+++ 实战任务3/3.5.py
@@ -0,0 +1,14 @@
+# 开发人员    :  Yu
+# 开发时间    : 2024/1/12  11:27
+# 文件名称    : 3.5.PY
+# 开发工具    : PyCharm
+print("两 全    美")
+print("     乐")
+print("     无")
+print("     穷")
+word=input("请输入所缺词语: ")
+print(word)
+print("\n 两 全\033[1;31m",word,"\033[0m 美")
+print("      乐")
+print("      无")
+print("      穷")
