7497c205c1ca626f1ca330501ce668c01e548617
Yu'sDesktop <1526892925@qq.com>
2024-01-17 14:03:02 +0800

    困

---
+++ 实战项目7/截取字符串.py
@@ -0,0 +1,10 @@
+# 开发人员    :  Yu
+# 开发时间    : 2024/1/17  13:56
+# 文件名称    : 截取字符串.PY
+# 开发工具    : PyCharm
+str1='人生苦短，我用Python！'
+substr1=str1[1]
+substr2=str1[5:]
+substr3=str1[:5]
+substr4=str1[2:5]
+print(substr1+'\n'+substr2+'\n'+substr3+'\n'+substr4)
---
+++ 实战项目7/拼接字符串.py
@@ -0,0 +1,16 @@
+# 开发人员    :  Yu
+# 开发时间    : 2024/1/17  13:50
+# 文件名称    : 拼接字符串.PY
+# 开发工具    : PyCharm
+mot_en='Remembrance is a form of meeting.'
+mot_cn='记忆是一种相遇。'
+print(mot_en+mot_cn)
+str1='今天我走了'
+num=12098
+str2='步'
+print(str1+str(num)+str2)
+str3='人生苦短，我用python'
+length=len(str3)
+print(length)
+length2=len(str3.encode())
+print(length2)
